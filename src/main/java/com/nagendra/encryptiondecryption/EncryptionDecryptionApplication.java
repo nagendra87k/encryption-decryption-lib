e67cc6599c9f81e4b603a6f9343632aac7ce6157
nagendrakumar <inbox.nagendra@gmail.com>
2022-01-23 02:14:22 +0530

    Initial project setup

---
+++ src/main/java/com/nagendra/encryptiondecryption/EncryptionDecryptionApplication.java
@@ -0,0 +1,13 @@
+package com.nagendra.encryptiondecryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionDecryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionDecryptionApplication.class, args);
+	}
+
+}
