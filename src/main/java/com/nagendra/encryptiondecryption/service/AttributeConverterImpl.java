ca77b808ed49289c34cf0a68b2401bb12796d91e
nagendrakumar <inbox.nagendra@gmail.com>
2022-01-24 12:45:07 +0530

    Feat: Initial setup

--- src/main/java/com/nagendra/encryptiondecryption/service/AttributeConverterImpl.java
+++ src/main/java/com/nagendra/encryptiondecryption/service/AttributeConverterImpl.java
@@ -1,5 +1,7 @@
 package com.nagendra.encryptiondecryption.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.BadPaddingException;
@@ -7,32 +9,27 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
-import java.io.UnsupportedEncodingException;
 import java.security.Key;
 import java.util.Base64;
 
 @Component
+@PropertySource({"classpath:/application.properties"})
 public class AttributeConverterImpl implements AttributeConverter<String, String> {
 
     private static final String AES = "AES";
 
-   /* @Value("${key.enc}")
-    private static String encyKey;*/
+    public static String key;
 
-    private static byte[] encryptionKey = new byte[0];
-
-    static {
-        try {
-            encryptionKey = "0123456789abcdφ".getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+    @Value("${key.enc}")
+    public static void setKey(String key) {
+        AttributeConverterImpl.key = key;
     }
 
     private final Cipher encryptCipher;
     private final Cipher decryptCipher;
 
     public AttributeConverterImpl() throws Exception {
+        byte[] encryptionKey = key.getBytes("UTF-8");
         Key key = new SecretKeySpec(encryptionKey, AES);
         encryptCipher = Cipher.getInstance(AES);
         encryptCipher.init(Cipher.ENCRYPT_MODE, key);
